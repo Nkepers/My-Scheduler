07774aeec906c4be363937b8eb9159349ed9fa76
Nkepers <nick.kepers.coder@gmail.com>
2021-10-16 21:57:19 -0700

    coded the colors in as well as local storage.

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
 $('#currentDay').text(moment().format("MMM Do YY"));
-const saveButton = $('.saveBtn');
+var saveButton = $('.saveBtn');
 
-const timeOfDay = function(){
-    const timeArray = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
+var timeOfDay = function () {
+    var timeArray = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
+    var hour = moment().hour();
 
-    // const textSpace = document.getElementsByClassName('description');
-
-    let hour = moment().hour();
-
-    for (const i = 0; i < timeArray.length; i++) {
-        const Id = '#' + timeArray[i];
-        $(Id).val(localStorage.getItem(timeArray[i]));
+    for (var i = 0; i < timeArray.length; i++) {
+        var hourId = '#' + timeArray[i];
+        $(hourId).val(localStorage.getItem(timeArray[i]));
         if (timeArray[i] > hour) {
-            $(Id).addClass('bg-success');
+            $(hourId).addClass('bg-success');
         }
         else if (timeArray[i] < hour) {
-            $(Id).addClass('bg-info');
+            $(hourId).addClass('bg-danger');
         }
-        // textSpace.appendChild(hourId);
     }
 }
 
-const storeTask = function() {
-    const task = $(this).siblings('.description').val();
-    const hourInfo = $(this).siblings('.description').attr('id');
+var storeTask = function () {
+    var task = $(this).siblings('.description').val();
+    var hourInfo = $(this).siblings('.description').attr('id');
 
     console.log(hourInfo)
     console.log('Button Clicked')
@@ -44,4 +28,4 @@ const storeTask = function() {
 }
 
 saveButton.on('click', storeTask);
-storeTask();
+timeOfDay();
